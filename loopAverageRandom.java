ce88c1426a3f778998e26c19a79b133c418ada0b
jagaither <jgaither@tacc.utexas.edu>
2021-06-18 13:45:59 -0500

    Update loopAverageRandom.java

--- loopAverageRandom.java
+++ loopAverageRandom.java
@@ -10,9 +10,9 @@ public class loopAverageRandom {
 	private final static int milHold = 1000000;
 	static long timeSum = 0;
 	static long timeAverage = 0;
-    int remainder = 0;
+	int remainder = 0;
 	static int maxIndiCount = 0;
-    static int countListIndex = 0;
+	static int countListIndex = 0;
 	private static int originalN = 0;
 	
 	static boolean flag = false;
@@ -42,7 +42,7 @@ public class loopAverageRandom {
 		for(int k=0; k<numLoops; k++) {
 			int numArr[] = new int[milHold];
 			int sumArr[] = new int[N];
-		    int randHoldArr[] = new int[N];
+			int randHoldArr[] = new int[N];
 			
 			List<Integer> countList = new ArrayList<Integer>();		//Initialize countList so that each thread can maintain its own count to 
 			int incrementVal = 0;
@@ -52,38 +52,38 @@ public class loopAverageRandom {
 			}
 			
 			Thread[] myThreads = new Thread[N];					
-		    long tik = System.nanoTime();
-		    for(int i=0; i<N; i++){
-		    	countListIndex = i;
-			    myThreads[i] = new Thread(new Runnable(){			//creating an array of size N of threads to enter random integers into numList
-				    public void run(){
-					    while(countList.get(countListIndex) < maxIndiCount){
-						    randHoldArr[countListIndex] = ThreadLocalRandom.current().nextInt(); //using thread local random and an array matching countList so each 
-						    numArr[countList.get(countListIndex)] = randHoldArr[countListIndex]; //each thread has its own memory location for entry of the random number
-						    sumArr[countListIndex] += randHoldArr[countListIndex];	
-						    countList.set(countListIndex, countList.get(countListIndex)+1);
-					    }
-				    }
-			    });
+			long tik = System.nanoTime();
+			for(int i=0; i<N; i++){
+				countListIndex = i;
+				myThreads[i] = new Thread(new Runnable(){			//creating an array of size N of threads to enter random integers into numList
+						public void run(){
+							while(countList.get(countListIndex) < maxIndiCount){
+								randHoldArr[countListIndex] = ThreadLocalRandom.current().nextInt(); //using thread local random and an array matching countList so each 
+								numArr[countList.get(countListIndex)] = randHoldArr[countListIndex]; //each thread has its own memory location for entry of the random number
+								sumArr[countListIndex] += randHoldArr[countListIndex];	
+								countList.set(countListIndex, countList.get(countListIndex)+1);
+							}
+						}
+				});
 			
-			    myThreads[i].start();							
+				myThreads[i].start();							
 			
-			    try {												//joining threads to halt unwanted premature execution of runnable
-			        myThreads[i].join();							//which would lead to count not being correclty incremented and 
-			    } catch (InterruptedException e){					//therefore the array list as well
-				    // TODO Auto-generated catch block
-				    e.printStackTrace();
-			    }
-		    }
-		    long tok = System.nanoTime();
+				try {												//joining threads to halt unwanted premature execution of runnable
+					myThreads[i].join();							//which would lead to count not being correclty incremented and 
+				} catch (InterruptedException e){					//therefore the array list as well
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+			long tok = System.nanoTime();
             //-------------------------------------------------------------------------
-            // BEGINNING OF TIMING PORTION
+            // END OF TIMING PORTION
             //-------------------------------------------------------------------------
 		    
 		    
-		    long time = tok-tik;
+			long time = tok-tik;
 		    timeSum += time;
-            long average = 0;								//obtain average from sum
+		    long average = 0;								//obtain average from sum
 		    for(int j=0; j<N; j++) {
 		    	average+=sumArr[j];
 		    }
@@ -94,6 +94,7 @@ public class loopAverageRandom {
 		    System.out.println("Average value of random list is " + average);
 		    System.out.println("Execution time of data entry is " + time + " nanoseconds, or " + time/milHold + " milliseconds" + '\n');
 		}
+		
 		timeSum = timeSum/milHold;									//Get time sum in nanoseconds to milliseconds
 		timeAverage = timeSum/numLoops;
 		System.out.println('\n' + "Average time over 100 runs is " + timeAverage + " milliseconds");
